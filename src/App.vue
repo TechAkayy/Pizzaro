ad80703713c90960f6173ca0e57d427e6810f2fa
akkayy <techakayy@gmail.com>
2022-06-28 19:43:51 +1000

    Made navbar responsive

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,24 @@
 	@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;700;900&family=Roboto:wght@100;300;400;500;700;900&display=swap');
 </style>
 <template>
-	<v-app style="background: #E8E7E7;">
+	<v-app style="background: #e8e7e7">
 		<!-- <v-app-bar extension-height="48" height="64" model-value></v-app-bar> -->
-		<v-app-bar flat height="64" location="top" tag="header" color="transparent">
-			<v-tabs style="width: 100%" centered :model-value="currentRoute" hide-slider color="error">
-				<v-tab title="Track Order" class="text-subtitle-2" prepend-icon="material-symbols-search-rounded" to="/track" selected-class="v-tab--selected" value="track"></v-tab>
-				<v-tab to="/" selected-class="v-tab--selected" value="home">
-					<v-img src="src/assets/logo.svg" width="75"></v-img>
-				</v-tab>
-				<v-tab title="My Cart" class="text-subtitle-2" prepend-icon="noto-pizza" to="/order" selected-class="v-tab--selected" value="order"></v-tab>
-			</v-tabs>
+		<v-app-bar flat color="transparent" height="64" location="top" tag="header"><div class="d-flex justify-center" style="width: 100%;">
+                <v-tabs :model-value="currentRoute" hide-slider color="error">
+                    <v-tab title="Track Order" class="d-none d-sm-flex hidden-only text-subtitle-2" prepend-icon="material-symbols-search-rounded" to="/track" value="track" selected-class="v-tab--selected"></v-tab>
+                    <v-tab to="/track" value="track" class="d-sm-none text-h6" icon="material-symbols-search-rounded" selected-class="v-tab--selected"></v-tab>
+                    <v-tab to="/" value="home" selected-class="v-tab--selected">
+                        <v-img src="src/assets/logo.svg" width="75"></v-img>
+                    </v-tab>
+                    <v-tab class="d-none d-sm-flex hidden-only text-subtitle-2" to="/order" value="order" prepend-icon="noto-pizza" selected-class="v-tab--selected">My Cart
+                        <v-chip density="compact" color="error" size="small" variant="flat" rounded tag="span">6</v-chip>
+                    </v-tab>
+                    <v-tab to="/order" value="order" class="d-sm-none text-subtitle-2" selected-class="v-tab--selected">My Cart
+                        <v-chip density="compact" color="error" size="small" variant="flat" class=" rounded" tag="span">6</v-chip>
+                    </v-tab>
+                </v-tabs>
+            </div>
+			
 		</v-app-bar>
 		<v-main>
 			<router-view/>
