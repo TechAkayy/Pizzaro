999015d339ed4b3355e77455a6a1e7adce6b845b
akkayy <techakayy@gmail.com>
2022-06-29 19:16:37 +1000

    Added VSelect customTypes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
 import { liveDesigner } from '@pinegrow/vite-plugin'
-import { pinegrowVuetifyPlugin } from "@pinegrow/vuetify-plugin";
+import { pinegrowVuetifyPlugin } from '@pinegrow/vuetify-plugin'
 import Unocss from 'unocss/vite'
 import presetIcons from '@unocss/preset-icons'
 import Components from 'unplugin-vue-components/vite'
@@ -16,7 +16,33 @@ export default defineConfig({
 	plugins: [
 		liveDesigner({
 			plugins: [pinegrowVuetifyPlugin],
-			iconsets: ['all']
+			iconsets: ['all'],
+			customTypes: {
+				// To apply type overrides to a specific component (overrides above default fn),
+				// ComponentName: { prop: { customType: 'icon' } },
+				VSelect: {
+					variant: {
+						customType: 'select',
+						options: ['plain', 'outlined', 'underlined', 'solo'],
+						default: ''
+					},
+					density: {
+						customType: 'select',
+						options: ['default', 'compact', 'comfortable']
+					},
+					disabled: {
+						customType: 'boolean'
+					},
+					appendIcon: {
+						customType: 'icon'
+					},
+					prependIcon: {
+						customType: 'icon'
+					}
+				}
+				// To apply type overrides to all components,
+				// all: { prop: { customType: 'icon' } },
+			}
 		}),
 		vue(),
 		Unocss({
