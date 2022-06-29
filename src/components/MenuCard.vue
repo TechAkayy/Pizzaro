6ebca3f402a3460287194d96bdfd48304f4bf8ad
akkayy <techakayy@gmail.com>
2022-06-29 22:11:11 +1000

    Minor cleanup

--- src/components/MenuCard.vue
+++ src/components/MenuCard.vue
@@ -39,54 +39,88 @@
 
 <template>
 	<v-card class="pb-3 pt-3" elevation="0" border>
-		<v-img width="120px" class="ml-auto mr-auto" :src="pizza.img"></v-img><v-card-title class="justify-center text-subtitle-2">{{
+		<v-img width="120px" class="ml-auto mr-auto" :src="pizza.img"></v-img
+		><v-card-title class="justify-center text-subtitle-2">{{
 			pizza.name
-		}}</v-card-title><v-card-subtitle class="align-start d-flex text-sm-caption" style="min-height: 60px;">{{
-			pizza.ingredients
-		}}</v-card-subtitle><v-divider class="mb-2 ml-3 mr-3 mt-2"></v-divider>
+		}}</v-card-title
+		><v-card-subtitle
+			class="align-start d-flex text-sm-caption"
+			style="min-height: 60px"
+			>{{ pizza.ingredients }}</v-card-subtitle
+		><v-divider class="mb-2 ml-3 mr-3 mt-2"></v-divider>
 		<div class="d-flex justify-center">
 			<table class="ml-3 mr-3">
 				<thead>
 					<tr>
-						<th class="pl-2 pr-2 text-caption text-center" v-for="(size, index) in sizes" :key="index">
+						<th
+							class="pl-2 pr-2 text-caption text-center"
+							v-for="(size, index) in sizes"
+							:key="index"
+						>
 							{{ size.name }}
 						</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td class="pl-2 pr-2 text-center text-subtitle-2" v-for="(size, index) in sizes" :key="index">
-							<span :class="
+						<td
+							class="pl-2 pr-2 text-center text-subtitle-2"
+							v-for="(size, index) in sizes"
+							:key="index"
+						>
+							<span
+								:class="
 									pizza.size_on_special && pizza.size_on_special === size.name
 										? 'text-red'
 										: ''
-								">${{
+								"
+								>${{
 									pizza.size_on_special && pizza.size_on_special === size.name
 										? size.special_price
 										: size.price
-								}}</span>
+								}}</span
+							>
 						</td>
 					</tr>
 				</tbody>
 			</table>
 		</div>
 		<v-divider class="mb-2 ml-3 mr-3 mt-2"></v-divider>
+		<!-- <v-select
+			:items="sizes"
+			class="ml-2 text-caption"
+			item-title="description"
+			item-value="name"
+			v-model="selectedSize"
+			variant="outlined"
+			hide-details
+			density="compact"
+		></v-select> -->
 
 		<v-card-actions class="justify-center mt-n2">
-			<!-- <v-select
-				:items="sizes"
-				class="ml-2 text-caption"
-				item-title="description"
-				item-value="name"
-				v-model="selectedSize"
-				variant="outlined"
-			></v-select> -->
 			<div class="align-center d-flex flex-nowrap mt-2">
 				<div class="ml-2">
-					<vue-select v-model="selectedSize" :options="sizes" label="description" :value="sizes[0]" append-to-body :clearable="false" :searchable="false" style="min-width: 150px"></vue-select>
+					<vue-select
+						v-model="selectedSize"
+						:options="sizes"
+						label="description"
+						:value="sizes[0]"
+						append-to-body
+						:clearable="false"
+						:searchable="false"
+						style="min-width: 150px"
+					></vue-select>
 				</div>
-				<v-btn class="d-sm-none ml-2 mr-2" icon="mdi-plus-box" color="error" variant="tonal">Add</v-btn>
-				<v-btn class="bg-error d-none d-sm-block ml-2" density="compact">Add</v-btn>
+				<v-btn
+					class="d-sm-none ml-2 mr-2"
+					icon="mdi-plus-box"
+					color="error"
+					variant="tonal"
+					>Add</v-btn
+				>
+				<v-btn class="bg-error d-none d-sm-block ml-2" density="compact"
+					>Add</v-btn
+				>
 			</div>
 		</v-card-actions>
 	</v-card>
