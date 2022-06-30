a9319c6cdea8c5dae7a311f2bce389ee9dce381e
akkayy <techakayy@gmail.com>
2022-07-01 00:59:07 +1000

    Minor fix on restructuring pizzas and specials state

--- src/components/PromoCard.vue
+++ src/components/PromoCard.vue
@@ -8,15 +8,28 @@
 </script>
 
 <template>
-	<v-card class=" pb-3 pt-3" elevation="0" border>
-		<v-img width="120px" class=" ml-auto mr-auto" :src="special.img"></v-img><v-card-title class="justify-center text-subtitle-2">{{
+	<v-card class="pb-3 pt-3" elevation="0" border>
+		<v-img width="120px" class="ml-auto mr-auto" :src="special.img"></v-img
+		><v-card-title class="justify-center text-subtitle-2">{{
 			special.name
 		}}</v-card-title>
-		<v-card-title class=" justify-center text-subtitle-2">
-			<span class="font-weight-bold mr-2 text-error text-subtitle-1">${{ special.selectedSize.special_price }}</span>
-			<span class=" font-weight-bold text-caption text-decoration-line-through">${{ special.selectedSize.price }}</span>
+		<v-card-title class="justify-center text-subtitle-2">
+			<span class="font-weight-bold mr-2 text-error text-subtitle-1"
+				>${{ special.special_price }}</span
+			>
+			<span class="font-weight-bold text-caption text-decoration-line-through"
+				>${{ special.price }}</span
+			>
 		</v-card-title>
-		<div class="d-flex justify-center"><v-card-subtitle>{{special.size_on_special}}</v-card-subtitle><v-btn icon="mdi-plus-box" class="d-block d-sm-none ml-2 text-error">Add</v-btn><v-btn class="d-none d-sm-block " color="error" density="compact">Add</v-btn></div><v-col></v-col>
+		<div class="d-flex justify-center">
+			<v-card-subtitle>{{ special.size_on_special }}</v-card-subtitle
+			><v-btn icon="mdi-plus-box" class="d-block d-sm-none ml-2 text-error"
+				>Add</v-btn
+			><v-btn class="d-none d-sm-block" color="error" density="compact"
+				>Add</v-btn
+			>
+		</div>
+		<v-col></v-col>
 	</v-card>
 </template>
 
