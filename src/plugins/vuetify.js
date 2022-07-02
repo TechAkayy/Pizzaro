ca78186f7fb4a5aea7eb6277efb349f9a3647ea5
akkayy <techakayy@gmail.com>
2022-07-03 00:45:56 +1000

    Applied theme in vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,7 +8,31 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
+const myCustomLightTheme = {
+	dark: false,
+	colors: {
+		background: '#FFFFFF',
+		surface: '#FFFFFF',
+		primary: '#6200EE',
+		'primary-darken-1': '#3700B3',
+		secondary: '#03DAC6',
+		'secondary-darken-1': '#018786',
+		error: '#B00020',
+		info: '#2196F3',
+		success: '#4CAF50',
+		warning: '#FB8C00'
+	}
+}
+
 export default createVuetify({
+	// https://next.vuetifyjs.com/en/features/theme/#setup
+	theme: {
+		defaultTheme: 'myCustomLightTheme',
+		themes: {
+			myCustomLightTheme
+		}
+	},
+
 	icons: {
 		defaultSet: 'class',
 		aliases: Object.keys(aliases).reduce((obj, key) => {
