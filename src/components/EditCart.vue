dbca23110a827dcf1b6cbdffce6bbe990d6cfdba
akkayy <techakayy@gmail.com>
2022-07-04 18:39:15 +1000

    Removed EditCart from Home.vue

--- src/components/EditCart.vue
+++ src/components/EditCart.vue
@@ -21,103 +21,98 @@ const applyCode = () => {
 
 </script>
 <template class="">
-  <v-card class="pa-2" style="height: 100%;">
-    <v-container class="cart-width">
-      <v-card-title>
-        <v-icon icon="noto-pizza" size="x-small"></v-icon>
-        <span class="ml-1 text-no-wrap">My Cart</span>
-        <v-spacer></v-spacer>
-        <v-text-field hide-details placeholder="Discount Code" density="compact" variant="outlined" v-model="code"
-          class="" style="max-width: 200px;"></v-text-field>
-        <v-btn color="error" class="ml-2" @click="applyCode">Apply</v-btn>
-      </v-card-title>
-      <v-table>
-        <tbody>
-          <tr v-for="(item, index) in cartItems" :key="index">
-            <td>
-              <v-btn icon="fluent-delete-20-regular" density="compact" variant="flat"
-                @click="removeFromCart(item.name)"></v-btn>
-            </td>
-            <td>
-              <v-img width="35" :src="item.img"></v-img>
-            </td>
-            <td>
-              <div class="text-no-wrap text-subtitle-2">{{ item.name }}</div>
-              <div class="text-caption">
-                {{ item.selectedSize.description }}
-              </div>
-            </td>
-            <td>
-              <v-btn icon="icon-park-outline-reduce-one" density="compact" variant="flat"
-                @click="decrementCartItemCount(index)"></v-btn>
-            </td>
-            <td>
-              <div>{{ item.count }}</div>
-            </td>
-            <td>
-              <v-btn icon="icon-park-outline-add-one" density="compact" variant="flat"
-                @click="incrementCartItemCount(index)"></v-btn>
-            </td>
-            <td>
-              <div class="font-weight-bold">${{ item.countPrice }}</div>
-            </td>
-          </tr>
-          <tr class="d-none">
-            <td colspan="7" class="pl-0 pr-0">
-              <v-divider></v-divider>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="6">
-              <div class="text-caption">
-                Delivery Fee (free for orders over $50)
-              </div>
-            </td>
-            <td>
-              <div class="font-weight-bold">${{ orderAmounts.deliveryFee }}</div>
-            </td>
-          </tr>
-          <tr v-if="discountCode">
-            <td colspan="6">
-              <div class="text-caption">Discount (10% off)</div>
-            </td>
-            <td>
-              <div class="font-weight-bold">${{ orderAmounts.discount }}</div>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="6">
-              <div class="text-caption">Subtotal</div>
-            </td>
-            <td>
-              <div class="font-weight-bold">${{ orderAmounts.subTotal }}</div>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="6">
-              <div class="text-caption">GST (10%)</div>
-            </td>
-            <td>
-              <div class="font-weight-bold">${{ orderAmounts.gst }}</div>
-            </td>
-          </tr>
-          <tr class="d-none">
-            <td colspan="7" class="pl-0 pr-0">
-              <v-divider></v-divider>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="6">
-              <div class="font-weight-bold text-body-1">Total</div>
-            </td>
-            <td>
-              <div class="font-weight-bold">${{ orderAmounts.total }}</div>
-            </td>
-          </tr>
-        </tbody>
-      </v-table>
-    </v-container>
-  </v-card>
+    <v-card class="pa-2">
+        <v-container class="cart-width">
+            <v-card-title>
+                <v-icon icon="noto-pizza" size="x-small"></v-icon><span class="ml-1 text-no-wrap">My Cart</span>
+                <v-spacer></v-spacer>
+                <v-text-field hide-details placeholder="Discount Code" density="compact" variant="outlined" v-model="code" class="" style="max-width: 200px;"></v-text-field>
+                <v-btn color="error" class="ml-2" @click="applyCode">Apply</v-btn>
+            </v-card-title>
+            <v-table>
+                <tbody>
+                    <tr v-for="(item, index) in cartItems" :key="index">
+                        <td>
+                            <v-btn icon="fluent-delete-20-regular" density="compact" variant="flat" @click="removeFromCart(item.name)"></v-btn>
+                        </td>
+                        <td>
+                            <v-img width="35" :src="item.img"></v-img>
+                        </td>
+                        <td>
+                            <div class="text-no-wrap text-subtitle-2">{{ item.name }}</div>
+                            <div class="text-caption">
+                                {{ item.selectedSize.description }}
+</div>
+                        </td>
+                        <td>
+                            <v-btn icon="icon-park-outline-reduce-one" density="compact" variant="flat" @click="decrementCartItemCount(index)"></v-btn>
+                        </td>
+                        <td>
+                            <div>{{ item.count }}</div>
+                        </td>
+                        <td>
+                            <v-btn icon="icon-park-outline-add-one" density="compact" variant="flat" @click="incrementCartItemCount(index)"></v-btn>
+                        </td>
+                        <td>
+                            <div class="font-weight-bold">${{ item.countPrice }}</div>
+                        </td>
+                    </tr>
+                    <tr class="d-none">
+                        <td colspan="7" class="pl-0 pr-0">
+                            <v-divider></v-divider>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="6">
+                            <div class="text-caption">
+                                Delivery Fee (free for orders over $50)
+</div>
+                        </td>
+                        <td>
+                            <div class="font-weight-bold">${{ orderAmounts.deliveryFee }}</div>
+                        </td>
+                    </tr>
+                    <tr v-if="discountCode">
+                        <td colspan="6">
+                            <div class="text-caption">Discount (10% off)</div>
+                        </td>
+                        <td>
+                            <div class="font-weight-bold">${{ orderAmounts.discount }}</div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="6">
+                            <div class="text-caption">Subtotal</div>
+                        </td>
+                        <td>
+                            <div class="font-weight-bold">${{ orderAmounts.subTotal }}</div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="6">
+                            <div class="text-caption">GST (10%)</div>
+                        </td>
+                        <td>
+                            <div class="font-weight-bold">${{ orderAmounts.gst }}</div>
+                        </td>
+                    </tr>
+                    <tr class="d-none">
+                        <td colspan="7" class="pl-0 pr-0">
+                            <v-divider></v-divider>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="6">
+                            <div class="font-weight-bold text-body-1">Total</div>
+                        </td>
+                        <td>
+                            <div class="font-weight-bold">${{ orderAmounts.total }}</div>
+                        </td>
+                    </tr>
+                </tbody>
+            </v-table>
+        </v-container>
+    </v-card>
 </template>
 <style>
 @media (min-width:1264px) {
--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -17,36 +17,34 @@ const submitOrder = () => {
 
 </script>
 <template>
-  <v-container class="pl-0">
-    <v-btn prepend-icon="material-symbols-arrow-back-rounded" variant="text" class="font-weight-bold text-subtitle-2"
-      to="/">Back to Menu</v-btn>
-  </v-container>
-  <v-container>
-    <p class="font-weight-bold mt-1 mt-md-3 mt-sm-2 text-h5 text-md-h3 text-sm-h4"
-      style="font-family: 'Playfair Display', serif !important">
-      My Order </p>
-  </v-container>
-  <v-form ref="form" lazy-validation v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col cols="12" md="false" lg="12" xl="5">
-          <EditCart />
-        </v-col>
-        <v-col cols="12" md="false" lg="false" xl="7" v-if="count !== 0">
-          <v-row>
-            <v-col cols="12" md="6" lg="false" xl="6">
-              <EditDeliveryInfo />
-            </v-col>
-            <v-col cols="12" md="6" lg="false" xl="6">
-              <EditPaymentMethod @submit-order="submitOrder" />
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col cols="12" md="4">
-        </v-col>
-      </v-row>
+    <v-container class="pl-0">
+        <v-btn prepend-icon="material-symbols-arrow-back-rounded" variant="text" class="font-weight-bold text-subtitle-2" to="/">Back to Menu</v-btn>
     </v-container>
-  </v-form>
+    <v-container>
+        <p class="font-weight-bold mt-1 mt-md-3 mt-sm-2 text-h5 text-md-h3 text-sm-h4" style="font-family: 'Playfair Display', serif !important">
+      My Order </p>
+    </v-container>
+    <v-form ref="form" lazy-validation v-model="valid">
+        <v-container>
+            <v-row>
+                <v-col cols="12" md="false" lg="12" xl="5">
+                    <EditCart/>
+                </v-col>
+                <v-col cols="12" md="false" lg="false" xl="7" v-if="count !== 0">
+                    <v-row>
+                        <v-col cols="12" md="6" lg="false" xl="6">
+                            <EditDeliveryInfo/>
+                        </v-col>
+                        <v-col cols="12" md="6" lg="false" xl="6">
+                            <EditPaymentMethod @submit-order="submitOrder"/>
+                        </v-col>
+                    </v-row>
+                </v-col>
+                <v-col cols="12" md="4">
+</v-col>
+            </v-row>
+        </v-container>
+    </v-form>
 </template>
 <style>
 </style>
